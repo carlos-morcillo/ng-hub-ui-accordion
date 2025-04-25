9222bafe699435861ceccb7b6c60dfb0b1adf63a
Carlos Morcillo <carlos.morcillo@bosonit.com>
2025-04-25 12:29:41 +0200

    update directive selector from 'appAccordionPanelHeader' to 'hubAccordionPanelHeader'

--- src/lib/directives/accordion-panel-header.directive.ts
+++ src/lib/directives/accordion-panel-header.directive.ts
@@ -1,8 +1,8 @@
 import { Directive, TemplateRef } from '@angular/core';
 
 @Directive({
-  selector: '[appAccordionPanelHeader]',
+	selector: '[hubAccordionPanelHeader]'
 })
 export class AccordionPanelHeaderDirective {
-  constructor(public templateRef: TemplateRef<any>) {}
+	constructor(public templateRef: TemplateRef<any>) {}
 }
